e68fe97ca0bd80acaf216b5236269de5d018089b
grapesurgeon <49630612+grapesurgeon@users.noreply.github.com>
2019-06-16 19:23:00 +0700

    Add files via upload

--- jscript/popup.js
+++ jscript/popup.js
@@ -1,28 +1,25 @@
-//for cart
+// Get the modal
 var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
+// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+// When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
+// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-//for login
-var loginModal = document.getElementById('login');
-
-
-//close modal
+// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == loginModal) {
-        loginModal.style.display = "none";
-    }
-    else if (event.target == modal) {
-        modal.style.display = "none";
-    }
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
